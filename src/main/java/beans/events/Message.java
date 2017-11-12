cebd63f29493923d627af6ab43d477e592347486
Jérôme <jerome@fourmond.fr>
2017-11-12 23:43:10 +0100

    Added method countReactions

--- src/main/java/beans/events/Message.java
+++ src/main/java/beans/events/Message.java
@@ -5,75 +5,81 @@ import beans.Reaction;
 import java.util.List;
 
 public class Message extends Event {
-    private String user;
-    private String text;
-    private String subtype;
-    private String timestamp;
-    private List<Reaction> reactions;
-
-
-    public Message(String user, String channel, String text, String subtype, String timestamp, List<Reaction> reactions) {
-        super(channel);
-        this.user = user;
-        this.text = text;
-        this.subtype = subtype;
-        this.timestamp = timestamp;
-        this.reactions = reactions;
-    }
-
-    //  GETTERS
-    public String getUser() {
-        return user;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public String getSubtype() {
-        return subtype;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public List<Reaction> getReactions() {
-        return reactions;
-    }
-
-    //  SETTERS
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public void setSubtype(String subtype) {
-        this.subtype = subtype;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setReactions(List<Reaction> reactions) {
-        this.reactions = reactions;
-    }
-
-    //  METHODES
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "user='" + user + '\'' +
-                ", text='" + text + '\'' +
-                ", subtype='" + subtype + '\'' +
-                ", timestamp='" + timestamp + '\'' +
-                ", reactions=" + reactions +
-                ", channel='" + channel + '\'' +
-                '}';
-    }
+	private String user;
+	private String text;
+	private String subtype;
+	private String timestamp;
+	private List<Reaction> reactions;
+	
+	
+	public Message(String user, String channel, String text, String subtype, String timestamp, List<Reaction> reactions) {
+		super(channel);
+		this.user = user;
+		this.text = text;
+		this.subtype = subtype;
+		this.timestamp = timestamp;
+		this.reactions = reactions;
+	}
+	
+	//  GETTERS
+	public String getUser() {
+		return user;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public String getSubtype() {
+		return subtype;
+	}
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+	
+	public List<Reaction> getReactions() {
+		return reactions;
+	}
+	
+	//  SETTERS
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void setSubtype(String subtype) {
+		this.subtype = subtype;
+	}
+	
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public void setReactions(List<Reaction> reactions) {
+		this.reactions = reactions;
+	}
+	
+	//  METHODES
+	public int countReactions() {
+		int count = 0;
+		for(Reaction reaction : reactions)
+			count += reaction.getCount();
+		return count;
+	}
+	
+	@Override
+	public String toString() {
+		return "Message{" +
+				"user='" + user + '\'' +
+				", text='" + text + '\'' +
+				", subtype='" + subtype + '\'' +
+				", timestamp='" + timestamp + '\'' +
+				", reactions=" + reactions +
+				", channel='" + channel + '\'' +
+				'}';
+	}
 }
