2e6f9364a510856dd52e1d3547e733aa5fe7333b
Florentin <flo.noel@hotmail.fr>
2017-11-18 01:41:13 +0100

    rappatriement de certaines propriétés de base dans le bot

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -8,159 +8,195 @@ import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 
 @WebSocket
 public abstract class Bot {
-	private static final Logger Log = LogManager.getLogger(Bot.class);
+    private static final Logger Log = LogManager.getLogger(Bot.class);
 
-	// API SLACK
-	protected SlackAPI api;
+    // API SLACK
+    protected SlackAPI api = new SlackAPI(true);
 
-	//  INFORMATION SUR LE BOT
-	String id;
-	String name;
+    private static final String URL = "url";
+    private static final String ID = "id";
 
-	final CountDownLatch closeLatch;
-	Session session;
+    //  INFORMATION SUR LE BOT
+    String id;
+    private String url;
+    private String name;
 
-	//  INFORMATION DE DUREE
-	int duration;
-	private TimeUnit unit;
+    protected final CountDownLatch closeLatch = new CountDownLatch(1);
+    Session session;
 
-	protected Bot(SlackAPI api, String id, String botName) throws Exception {
-		Log.info("Création du bot : " + id + "(" + botName + ")");
-		this.api = api;
-		this.id = id;
-		this.name = botName;
+    //  INFORMATION DE DUREE
+    int duration;
+    private TimeUnit unit;
 
-		initialisation();
+    protected Bot(String botName) throws Exception {
+        Log.info("Création du bot : " + botName);
+        this.name = botName;
 
-		this.closeLatch = new CountDownLatch(1);
-	}
+        initialisation();
+    }
 
-	/**
-	 * Initialisation du {@link Bot}
-	 *
-	 * @throws Exception
-	 */
-	protected void initialisation() throws Exception {
-	}
+    /**
+     * Initialisation du {@link Bot}
+     *
+     * @throws Exception
+     */
+    protected void initialisation() throws Exception {
+        Map<String, String> map;
+        try {
+            map = api.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
-	/**
-	 * Connection du {@link WebSocket}
-	 *
-	 * @param session
-	 */
-	@OnWebSocketConnect
-	public void onConnect(Session session) {
-		Log.info("Connexion");
-		this.session = session;
-	}
+        url = map.get(URL);
+        id = map.get(ID);
+    }
 
-	/**
-	 * Réception d'un message
-	 *
-	 * @param message : message reçu
-	 */
-	@OnWebSocketMessage
-	public void onMessage(String message) {
-		Log.info("Message reçu : " + message);
-	}
+    /**
+     * Connection du {@link WebSocket}
+     *
+     * @param session
+     */
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        Log.info("Connexion");
+        this.session = session;
+    }
 
-	@OnWebSocketError
-	public void onError(Throwable t) {
-		Log.error("Error: " + t.getMessage());
-	}
+    /**
+     * Réception d'un message
+     *
+     * @param message : message reçu
+     */
+    @OnWebSocketMessage
+    public void onMessage(String message) {
+        Log.info("Message reçu : " + message);
+    }
 
-	/**
-	 * Fermeture du {@link WebSocket}
-	 *
-	 * @param statusCode
-	 * @param reason
-	 */
-	@OnWebSocketClose
-	public void onClose(int statusCode, String reason) {
-		Log.info("Fermeture de la connexion : " + statusCode + " - " + reason);
+    @OnWebSocketError
+    public void onError(Throwable t) {
+        Log.error("Error: " + t.getMessage());
+    }
 
-		this.session = null;
-		this.closeLatch.countDown();
-	}
+    /**
+     * Fermeture du {@link WebSocket}
+     *
+     * @param statusCode
+     * @param reason
+     */
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        Log.info("Fermeture de la connexion : " + statusCode + " - " + reason);
 
-	public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
-		this.duration = duration;
-		this.unit = unit;
-		return this.closeLatch.await(duration, unit);
-	}
+        this.session.close();
+        this.closeLatch.countDown();
+    }
 
-	/**
-	 * Construit un message à envoyer
-	 *
-	 * @param text      : texte à envoyer
-	 * @param channelId identifiant du {@link beans.channels.Channel} où envoyer le message
-	 * @return
-	 */
-	private Future<Void> buildMessage(String text, String channelId) {
-		return session.getRemote().sendStringByFuture("{ " +
-				"\"type\" : \"message\", " +
-				"\"text\" : \"" + text + "\"," +
-				"\"channel\" : \"" + channelId + "\"" +
-				"}");
+    public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
+        this.duration = duration;
+        this.unit = unit;
+        return this.closeLatch.await(duration, unit);
+    }
 
-	}
+    /**
+     * Construit un message à envoyer
+     *
+     * @param text      : texte à envoyer
+     * @param channelId identifiant du {@link beans.channels.Channel} où envoyer le message
+     * @return
+     */
+    private Future<Void> buildMessage(List<String> text, String channelId) {
+        String message = "{\"type\" : \"message\", \"text\" : \"";
+        for (String t : text) {
+            message = message.concat(t).concat("\\n");
+        }
+        message = message.concat("\",\"channel\" : \"" + channelId + "\"}");
+        return session.getRemote().sendStringByFuture(message);
+    }
 
-	/**
-	 * Envoie un message via le {@link WebSocket}
-	 *
-	 * @param text      : texte à envoyer
-	 * @param channelId : idenfiant du {@link beans.channels.Channel} où envoyer le message
-	 * @throws ExecutionException
-	 * @throws InterruptedException
-	 * @throws TimeoutException
-	 */
-	void sendMessage(String text, String channelId) throws ExecutionException, InterruptedException, TimeoutException {
-		Future<Void> fut = null;
-		// Envoi du message
-		try {
-			fut = buildMessage(text, channelId);
-			fut.get(2, TimeUnit.SECONDS);
-		} catch (ExecutionException | InterruptedException e) {
-			// L'envoi a échoué
-			e.printStackTrace();
-			throw (e);
-		} catch (TimeoutException e) {
-			// Timeout
-			e.printStackTrace();
-			fut.cancel(true);
-			throw (e);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw (e);
-		}
-	}
+    /**
+     * Méthode raccourci pour {@link Bot#sendMessage(List, String)} dans le cas d'un message d'une ligne.
+     */
+    void sendMessage(String text, String channelId) throws InterruptedException, ExecutionException, TimeoutException {
+        sendMessage(Arrays.asList(text), channelId);
+    }
 
-	/**
-	 * Teste si le {@link Bot} a été cité dans le message passé en paramètre
-	 *
-	 * @param M : le message
-	 * @return {@code true} si le bot a été cité dans le message, {@code false} sinon
-	 */
-	boolean hasBeenCited(Message M) {
-		return (M.getText().contains("<@" + id + ">") || M.getText().toLowerCase().contains(name.toLowerCase()));
-	}
+    /**
+     * Envoie un message via le {@link WebSocket}
+     *
+     * @param text      : texte à envoyer
+     * @param channelId : idenfiant du {@link beans.channels.Channel} où envoyer le message
+     * @throws ExecutionException
+     * @throws InterruptedException
+     * @throws TimeoutException
+     */
+    void sendMessage(List<String> text, String channelId) throws ExecutionException, InterruptedException, TimeoutException {
+        Future<Void> fut = buildMessage(text, channelId);
 
-	boolean isPublicChannel(String channelId) {
-		return channelId.charAt(0) == 'C';
-	}
+        // Envoi du message
+        try {
+            fut.get(2, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            // L'envoi a échoué
+            e.printStackTrace();
+            throw (e);
+        } catch (TimeoutException e) {
+            // Timeout
+            e.printStackTrace();
+            fut.cancel(true);
+            throw (e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw (e);
+        }
+    }
 
-	ChannelType getChannelTypeFromMessage(Message M) {
-		switch (M.getChannel().charAt(0)) {
-			case 'C':
-				return ChannelType.PUBLIC;
-			case 'D':
-				return ChannelType.DIRECT_MESSAGE;
-			default:
-				return ChannelType.PRIVATE;
-		}
-	}
+    /**
+     * Teste si le {@link Bot} a été cité dans le message passé en paramètre
+     *
+     * @param M : le message
+     * @return {@code true} si le bot a été cité dans le message, {@code false} sinon
+     */
+    boolean hasBeenCited(Message M) {
+        return (M.getText().contains("<@" + id + ">") || M.getText().toLowerCase().contains(name.toLowerCase()));
+    }
+
+    boolean isPublicChannel(String channelId) {
+        return channelId.charAt(0) == 'C';
+    }
+
+    ChannelType getChannelTypeFromMessage(Message M) {
+        switch (M.getChannel().charAt(0)) {
+            case 'C':
+                return ChannelType.PUBLIC;
+            case 'D':
+                return ChannelType.DIRECT_MESSAGE;
+            default:
+                return ChannelType.PRIVATE;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- src/main/java/runnable/GuyLauncher.java
+++ src/main/java/runnable/GuyLauncher.java
@@ -1,6 +1,5 @@
 package runnable;
 
-import api.SlackAPI;
 import bot.Guy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,62 +7,44 @@ import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 
 import java.net.URI;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class GuyLauncher {
-	private static final Logger Log = LogManager.getLogger(GuyLauncher.class);
+    private static final Logger Log = LogManager.getLogger(GuyLauncher.class);
 
-	private static SlackAPI api = new SlackAPI(true);
+    private static final int DURATION = 120;
+    private static final TimeUnit UNIT = TimeUnit.MINUTES;
 
-	private static final String URL = "url";
-	private static final String ID = "id";
+    public static void main(String args[]) {
 
-	private static final int DURATION = 120;
-	private static final TimeUnit UNIT = TimeUnit.MINUTES;
+        WebSocketClient client = new WebSocketClient();
+        Guy socket;
+        try {
+            socket = new Guy();
+        } catch (Exception e) {
+            //  Erreur lors de l'appel aux informations
+            e.printStackTrace();
+            return;
+        }
 
-	public static void main(String args[]) {
-		Map<String, String> map;
-		String url;
-		String id;
-		try {
-			map = api.connect();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
+        try {
+            client.start();
+            URI echoUri = new URI(socket.getUrl());
 
-		url = map.get(URL);
-		id = map.get(ID);
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            client.connect(socket, echoUri, request);
 
-		WebSocketClient client = new WebSocketClient();
-		Guy socket;
-		try {
-			socket = new Guy(api, id);
-		} catch (Exception e) {
-			//  Erreur lors de l'appel aux informations
-			e.printStackTrace();
-			return;
-		}
+            Log.info("Connecting to : " + echoUri);
 
-		try {
-			client.start();
-			URI echoUri = new URI(url);
-
-			ClientUpgradeRequest request = new ClientUpgradeRequest();
-			client.connect(socket, echoUri, request);
-
-			Log.info("Connecting to : " + echoUri);
-
-			socket.awaitClose(DURATION, UNIT);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				client.stop();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            socket.awaitClose(DURATION, UNIT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
